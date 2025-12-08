fd3fa19fc4e99bf69e8f3128a8c3186ada2e9e2f
joshhhhjoh <128190607+joshhhhjoh@users.noreply.github.com>
2025-12-07 22:03:54 -0600

    Delete second.lua

--- second.lua
+++
@@ -1,7 +0,0 @@
-local Http = game:GetService("HttpService")
-local WEBHOOK = _G.WEBHOOK
-Http:PostAsync(
-    WEBHOOK,
-    Http:JSONEncode({ content = "Teleporting to JobId: " .. Server.id }),
-    Enum.HttpContentType.ApplicationJson
-)
