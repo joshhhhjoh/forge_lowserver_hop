47e512214a491bc6bf41b6d2fc315b2baf7c8c9f
joshhhhjoh <128190607+joshhhhjoh@users.noreply.github.com>
2025-12-07 22:09:49 -0600

    Refactor server listing and teleportation logic

--- main.lua
+++ main.lua
@@ -2,26 +2,19 @@ local Http = game:GetService("HttpService")
 local TPS = game:GetService("TeleportService")
 local Api = "https://games.roblox.com/v1/games/"
 
+local _G.WEBHOOK = script.Parent.Parent.TextBox.Text
+
 local _place = 129009554587176
 local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
-local WEBHOOK = _G.WEBHOOK
-
 function ListServers(cursor)
-    local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-    return Http:JSONDecode(Raw)
+  local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+  return Http:JSONDecode(Raw)
 end
 
-local Server, Next
-repeat
-    local Servers = ListServers(Next)
-    Server = Servers.data[1]
-    Next = Servers.nextPageCursor
+local Server, Next; repeat
+  local Servers = ListServers(Next)
+  Server = Servers.data[1]
+  Next = Servers.nextPageCursor
 until Server
 
-Http:PostAsync(
-    WEBHOOK,
-    Http:JSONEncode({ content = "Teleporting to JobId: " .. Server.id }),
-    Enum.HttpContentType.ApplicationJson
-)
-
-TPS:TeleportToPlaceInstance(_place, Server.id, game.Players.LocalPlayer)
+TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
